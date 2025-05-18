68989122e624ad1d05817234427d8805e2285b2d
Mohammad <mbalyallpuri@gmail.com>
2025-05-18 20:08:27 +0500

    Complete AddProduct.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,9 @@ import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
 import SellerLogin from './components/seller/SellerLogin';
 import SellerLayout from './pages/seller/SellerLayout';
+import Orders from './pages/seller/Orders';
+import AddProduct from './pages/seller/AddProduct';
+import ProductList from './pages/seller/ProductList';
 
 const App = () => {
   const location = useLocation();
@@ -36,7 +39,11 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
-          <Route path="/seller" element={isSeller ? <SellerLayout/> : <SellerLogin/>} />
+          <Route path="/seller" element={isSeller ? <SellerLayout /> : <SellerLogin />}>
+  <Route index element={<AddProduct />} /> {/* Default child */}
+  <Route path="product-list" element={<ProductList />} />
+  <Route path="orders" element={<Orders />} />
+</Route>
           {/* Add more routes here */}
         </Routes>
       </div>
