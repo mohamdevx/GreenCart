b16c378e090830ad1522b4a10683e9cbee18d5a9
Mohammad <mbalyallpuri@gmail.com>
2025-05-31 18:38:26 +0500

    order api complete 8:10

---
+++ server/controllers/addressConroller.js
@@ -0,0 +1,31 @@
+
+
+
+
+// add address: /api/address/add
+
+import Address from '../models/Address.js';
+
+
+export const addAddress = async (req, res) => {
+    try {
+        const {address,userId}=req.body;
+        await Address.create(...address,userId);
+        res.json({success:true,message:"Address Added"});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message});
+    }
+}
+
+//get address: /api/address/get
+export const getAddress=async(req,res)=>{
+    try {
+        const {userId}=req.body;
+        const addresses=await Address.find({userId})
+        res.json({success:true,addresses});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message});
+    }
+}
